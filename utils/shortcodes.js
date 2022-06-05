673e6aff6cd91e775bc25841c7e68957ae3f0f25
Kilian Finger <hey@kilianfinger.com>
2022-06-05 18:33:13 +0200

    feat: improve images

--- utils/shortcodes.js
+++ utils/shortcodes.js
@@ -2,8 +2,28 @@ const path = require('path');
 const Image = require('@11ty/eleventy-img');
 
 module.exports = {
-  image: async ({ src, alt = '', sizes = '90vw', widths = [1920, 1280, 640, 320], class: className = '' }) => {
+  image: async ({
+    src,
+    alt = '',
+    sizes = '100vw',
+    widths = [300, 600],
+    sizeType,
+    loading = 'lazy',
+    class: className = '',
+  }) => {
     const extension = path.extname(src).slice(1).toLowerCase();
+
+    let heights;
+    if (sizeType) {
+      sizes = {
+        portrait: '(min-width: 1920px): 496px, (min-width: 664px): 30vw, 60vw',
+        projectsCover: '(min-width: 1400px) 600px, (min-width: 664px) 48vw, 98vw',
+      }[sizeType];
+      widths = {
+        portrait: [248, 496, 992],
+        projectsCover: [150, 300, 600, 1200],
+      }[sizeType];
+    }
 
     let metadata = await Image('./src/assets/images/' + src, {
       widths,
@@ -15,7 +35,7 @@ module.exports = {
     let imageAttributes = {
       alt,
       sizes,
-      loading: 'lazy',
+      loading,
       decoding: 'async',
       ...(className && { class: className }),
     };

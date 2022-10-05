fe898257783b765a060b7c81168f188e4cf3fd09
Kilian Finger <hey@kilianfinger.com>
2022-10-05 20:56:27 +0200

    feat: privacy improvements

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,4 +1,16 @@
 import 'lazysizes';
 import initializePortrait from './intro-portrait';
+import * as party from 'party-js';
 
 initializePortrait();
+
+const cookie = document.createElement('span');
+cookie.innerText = '🍪';
+cookie.style.fontSize = '24px';
+
+document.querySelector('.js-cookies').addEventListener('click', (event) => {
+  party.confetti(event.currentTarget, {
+    count: party.variation.range(20, 40),
+    shapes: cookie,
+  });
+});

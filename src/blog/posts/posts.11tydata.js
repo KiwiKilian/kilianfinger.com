dc6fc6ad24aba58163313de9f4366060f6b8b58c
Kilian Finger <hey@kilianfinger.com>
2023-06-17 14:11:07 +0200

    fix: remove frontmatter indents

--- src/blog/posts/posts.11tydata.js
+++ src/blog/posts/posts.11tydata.js
@@ -1,4 +1,15 @@
+const { readableDate } = require('../../../utils/filters');
+
 module.exports = {
   layout: 'layouts/post.njk',
   tags: ['posts'],
+  eleventyComputed: {
+    metaImage: {
+      path: './src/blog/posts/post.og.njk',
+      data: {
+        title: (data) => data.title,
+        date: (data) => readableDate(data.date),
+      },
+    },
+  },
 };

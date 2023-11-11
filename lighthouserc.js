2a7722352b66125b7cfd974af86a8095a8595e34
Kilian Finger <hey@kilianfinger.com>
2023-11-11 22:20:47 +0100

    ci: lighthouse add autodiscoverUrlBlocklist

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,6 +3,9 @@ module.exports = {
     upload: {
       target: 'temporary-public-storage',
     },
+    collect: {
+      autodiscoverUrlBlocklist: ['/404.html', '/imprint/index.html', '/privacy/index.html'],
+    },
     assert: {
       preset: 'lighthouse:no-pwa',
     },

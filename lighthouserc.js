e556585e0fa196c3def5d8b565e8c3e9cdf92bf2
Kilian Finger <hey@kilianfinger.com>
2023-11-12 21:32:44 +0100

    chore: improve lighthouse config

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,10 +4,13 @@ module.exports = {
       target: 'temporary-public-storage',
     },
     collect: {
-      autodiscoverUrlBlocklist: ['/404.html', '/imprint/index.html', '/privacy/index.html'],
+      autodiscoverUrlBlocklist: ['/404.html', '/blog/index.html', '/imprint/index.html', '/privacy/index.html'],
     },
     assert: {
       preset: 'lighthouse:no-pwa',
+      assertions: {
+        'bf-cache': 'off',
+      },
     },
   },
 };

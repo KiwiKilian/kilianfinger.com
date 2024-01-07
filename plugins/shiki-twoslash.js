34b70573518fa8656db74b1377288653ebf45782
Kilian Finger <hey@kilianfinger.com>
2024-01-07 17:51:29 +0100

    feat: improve shiki twoslash line stripping

--- plugins/shiki-twoslash.js
+++ plugins/shiki-twoslash.js
@@ -8,7 +8,7 @@ export default async function (eleventyConfig, options = {}) {
   const { highlighters } = await setupForFile(options);
 
   eleventyConfig.addMarkdownHighlighter((code, lang, fence) => {
-    code = code.replace(/\r?\n$/, ''); // strip trailing newline fed during code block parsing
+    code = code.replace(/\r?\n$/, '').replace(/^\r?\n/, ''); // Strip new lines at beginning/end
     return transformAttributesToHTML(code, [lang, fence].join(' '), highlighters, options);
   });
 }

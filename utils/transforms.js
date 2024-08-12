7fbeb3ccf67f9f2f818f7088d0f91ea337f1ea7a
Kilian Finger <hey@kilianfinger.com>
2024-08-12 22:58:33 +0200

    feat: bump dependencies

--- utils/transforms.js
+++ utils/transforms.js
@@ -1,4 +1,4 @@
-import cheerio from 'cheerio';
+import * as cheerio from 'cheerio';
 import htmlMinifier from 'html-minifier';
 
 const shouldTransformHTML = (outputPath) => outputPath && outputPath.endsWith('.html');
